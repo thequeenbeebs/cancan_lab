35373932e491db271320c13726455e64f4d7ba6a
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-25 09:16:57 -0400

    Remove Notes#new, not actually used

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,6 @@
 class NotesController < ApplicationController
   load_and_authorize_resource only: [:edit, :show, :update]
   
-  def new
-    render partial: 'form', locals: {note: Note.new}
-  end
-  
   def create
     note = Note.new(note_params)
     note.user = current_user
